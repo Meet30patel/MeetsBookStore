d53c2c13c1db6099bd3cb73efa632f67a37fd2c8
Meet30patel <113566773+Meet30patel@users.noreply.github.com>
2023-04-12 17:08:51 -0400

    update

--- MeetsBooks.DataAccess/Data/ApplicationDbContext.cs
+++ MeetsBooks.DataAccess/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using MeetsBooks.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -12,5 +13,7 @@ namespace MeetsBookstore.DataAccess.Data
             : base(options)
         {
         }
+        public DbSet<Category> Categories { get; set; }
+
     }
 }
